161519c37de26f2a92f68b8fdb85d45b29597edb
JIAN LI <raymond.li.jian@gmail.com>
2024-09-29 08:11:05 -0700

    Create pair.cpp

---
+++ pair.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// 1. include with
+#include <algorithm>
+// build with make_pair(a, b)
+
+
+int main() {
+    cout << "testing" << endl;
+    return 0;
+}
